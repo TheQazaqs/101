5f6af9d23d55479297c63f4c257d5ea75eae2536
TheQazaqs <hikoma501@gmail.com>
2024-11-22 17:53:43 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,5 +47,5 @@ function draw()
 	image(img,0,0);
 
 	//Write your code below here ...
-    rect(280, 100, 200, 500); // Example coordinates: adjust x, y, width, height as needed
+    rect(280, 100, 700, 550); // Example coordinates: adjust x, y, width, height as needed
 }
